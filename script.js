83c496d1728bdc70c8ed5b20688d8c4a072b87f1
Md.Shariful Islam Sheum <sharifulislamsheum@gmail.com>
2025-08-30 16:02:35 +0600

    added call history and a active clear button

---
+++ script.js
@@ -0,0 +1,9 @@
+function getFormattedTime() {
+  const now = new Date();
+  return now.toLocaleTimeString('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: true
+  });
+}
